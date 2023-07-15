66ff207353bad3e1ba3998fe6352592f8ec0c81c
Kasemsan <kasemsan.cho@gmail.com>
2023-07-15 20:20:15 +0700

    .

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import dataJSON from "../data.json"
+
+function App() {
+  console.log(dataJSON)
+
+  return (
+    <div className="h-screen w-screen bg-LightGray">
+      <div className="bg-White p-4"></div>
+    </div>
+  )
+}
+
+export default App
